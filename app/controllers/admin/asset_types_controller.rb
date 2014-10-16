338e7024c6c7091f796d27799e05973d6ddfc797
Charlie Maffitt <cmaffitt@highgroove.com>
2014-10-16 16:43:31 -0400

    Add ability to add and edit asset types

---
+++ app/controllers/admin/asset_types_controller.rb
@@ -0,0 +1,39 @@
+class Admin::AssetTypesController < ApplicationController
+  respond_to :html
+
+  def index
+    @asset_types = AssetType.all
+  end
+
+  def new
+    @asset_type = AssetType.new
+  end
+
+  def create
+    @asset_type = AssetType.new(asset_params)
+    if @asset_type.save
+      respond_with @asset_type, location: admin_asset_types_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @asset_type = AssetType.find(params[:id])
+  end
+
+  def update
+    @asset_type = AssetType.find(params[:id])
+    if @asset_type.update_attributes(asset_params)
+      respond_with @asset_type, location: admin_asset_types_path
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def asset_params
+    params.require(:asset_type).permit(:name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   
   namespace :admin do
     resources :assets
+    resources :asset_types
   end
 
   # Example resource route with options:
