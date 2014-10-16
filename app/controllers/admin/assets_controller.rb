a0533b31138f2565f392f108c141bf3385ca9dc3
Charlie Maffitt <cmaffitt@highgroove.com>
2014-10-16 15:55:23 -0400

    Add ability to create new asset in database

--- app/controllers/admin/assets_controller.rb
+++ app/controllers/admin/assets_controller.rb
@@ -1,7 +1,26 @@
 class Admin::AssetsController < ApplicationController
+  respond_to :html
 
   def index
     @assets = Asset.all
   end
 
+  def new
+    @asset = Asset.new
+  end
+
+  def create
+    @asset = Asset.new(asset_params)
+    if @asset.save
+      respond_with @asset, location: admin_assets_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def asset_params
+    params.require(:asset).permit(:id)
+  end
 end
