2c5e97bdac9dc36c8d676841df2b7ddcb285ea10
Charlie Maffitt <cmaffitt@highgroove.com>
2014-10-16 16:02:53 -0400

    Add admin ability to remove assets

--- app/controllers/admin/assets_controller.rb
+++ app/controllers/admin/assets_controller.rb
@@ -18,9 +18,22 @@ class Admin::AssetsController < ApplicationController
     end
   end
 
+  def edit
+    @asset = Asset.find(params[:id])
+  end
+
+  def update
+    @asset = Asset.find(params[:id])
+    if @asset.update_attributes(asset_params)
+      respond_with @asset, location: admin_assets_path
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def asset_params
-    params.require(:asset).permit(:id)
+    params.require(:asset).permit(:created_at)
   end
 end
