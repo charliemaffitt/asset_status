5cdf74cca91bfb2818c01edc21ca6ca0eaefed4b
Charlie Maffitt <cmaffitt@highgroove.com>
2014-10-16 16:05:50 -0400

    Add migration to create asset types (pulaski, mcleod, etc)

---
+++ db/migrate/20141016200431_create_asset_types.rb
@@ -0,0 +1,8 @@
+class CreateAssetTypes < ActiveRecord::Migration
+  def change
+    create_table :asset_types do |t|
+      t.string  :name
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141009211346) do
+ActiveRecord::Schema.define(version: 20141016200431) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "asset_types", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "assets", force: true do |t|
     t.datetime "created_at"
