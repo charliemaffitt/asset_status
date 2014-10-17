1930f0192cc1bf0976a24beb80c9dd66c8a1c063
Charlie Maffitt <cmaffitt@highgroove.com>
2014-10-16 23:06:27 -0400

    Delegate to avoid law of demeter

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -2,4 +2,6 @@ class Asset < ActiveRecord::Base
   belongs_to :asset_type, inverse_of: :assets
   validates :asset_type_id, presence: true
   validates :admin_id, presence: true
+
+  delegate :name, to: :asset_type, prefix: true
 end
