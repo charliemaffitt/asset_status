e0c0e17a57812fad6a6e4350e2825424ee4d5a8d
Charlie Maffitt <cmaffitt@highgroove.com>
2014-10-16 23:19:13 -0400

    Add status field to assets

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141017024705) do
+ActiveRecord::Schema.define(version: 20141017031833) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20141017024705) do
     t.integer  "borrower_id"
     t.integer  "admin_id"
     t.text     "description"
+    t.string   "status"
   end
 
   add_index "assets", ["admin_id"], name: "index_assets_on_admin_id", using: :btree
