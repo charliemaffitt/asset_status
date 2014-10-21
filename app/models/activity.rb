8bb761eeccba90ee9b97049b4b2d4ed7c8aa5df0
Charlie Maffitt <cmaffitt@highgroove.com>
2014-10-21 15:13:00 -0400

    Validates presence of location in activity

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,4 +2,5 @@ class Activity < ActiveRecord::Base
   belongs_to :user, inverse_of: :activities
   belongs_to :location, inverse_of: :activities
   validates :user_id, presence: true
+  validates :location, presence: true
 end
