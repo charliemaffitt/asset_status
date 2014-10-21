ea842f0bc5709660827ea77244a79f065a243f80
Charlie Maffitt <cmaffitt@highgroove.com>
2014-10-20 23:54:06 -0400

    Order frontend by type name

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -2,6 +2,6 @@ class AssetsController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    @assets = Asset.all
+    @assets = Asset.includes(:asset_type).order('asset_types.name')
   end
 end
