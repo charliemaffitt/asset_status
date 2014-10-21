6db0271832769d626e04fbe05f59f9ba3c51be83
Charlie Maffitt <cmaffitt@highgroove.com>
2014-10-20 22:35:00 -0400

    Add method to get investment total for each asset type

--- app/models/asset_type.rb
+++ app/models/asset_type.rb
@@ -2,4 +2,8 @@ class AssetType < ActiveRecord::Base
   has_many :assets, inverse_of: :asset_type
   validates :name, presence: true
   delegate :count, to: :assets, prefix: true
+
+  def investment_total
+    assets.pluck(:value).sum
+  end
 end
