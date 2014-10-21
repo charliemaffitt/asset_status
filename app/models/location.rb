c4ac528eb45a0675a6934d6008162c2f78c218d1
Charlie Maffitt <cmaffitt@highgroove.com>
2014-10-21 15:40:17 -0400

    Add hours total to locations

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,8 @@
 class Location < ActiveRecord::Base
   has_many :activities, inverse_of: :location
   validates :name, presence: true
+
+  def total_work_hours
+    activities.pluck(:hours).sum
+  end
 end
