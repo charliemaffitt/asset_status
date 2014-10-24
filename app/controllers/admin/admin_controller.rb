eec13ff56f02819df4415daf3a99bc60d836a3bd
Charlie Maffitt <cmaffitt@highgroove.com>
2014-10-24 00:13:25 -0400

    Filter non-admins away from admin console

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,4 +1,11 @@
 class Admin::AdminController < ApplicationController
   respond_to :html
   before_filter :authenticate_user!
+  before_filter :verify_admin
+
+  private
+
+  def verify_admin
+    redirect_to root_path unless current_user.admin? 
+  end
 end
