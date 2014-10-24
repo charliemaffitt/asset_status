ca8e802a9049d5aabac53120f43d93f487b1f910
Charlie Maffitt <cmaffitt@highgroove.com>
2014-10-24 13:13:41 -0400

    Configure frontend for json response

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -1,7 +1,9 @@
 class AssetsController < ApplicationController
   before_filter :authenticate_user!
+  respond_to :json, :html
 
   def index
     @assets = Asset.includes(:asset_type).order('asset_types.name')
+    respond_with @assets
   end
 end
