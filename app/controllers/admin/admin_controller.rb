31280a81e29e5bae64f7fa1b127ad2780847d030
Charlie Maffitt <cmaffitt@highgroove.com>
2014-10-24 00:15:17 -0400

    Not quite right

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -6,6 +6,6 @@ class Admin::AdminController < ApplicationController
   private
 
   def verify_admin
-    redirect_to root_path unless current_user.admin? 
+    redirect_to root_path unless current_user.admin == true
   end
 end
