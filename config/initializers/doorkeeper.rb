99d880cb9bd874363afbb58d08d935271104d06e
Charlie Maffitt <cmaffitt@highgroove.com>
2014-10-24 16:06:46 -0400

    Restrict access to oauth admin console to admins

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -16,14 +16,10 @@ Doorkeeper.configure do
   end
 
   # If you want to restrict access to the web interface for adding oauth authorized applications, you need to declare the block below.
-  # admin_authenticator do
-  #   # Put your admin authentication logic here.
-  #   if current_user.admin?
-  #     redirect_to oauth_applications_path
-  #   else
-  #     redirect_to new_user_session_path
-  #   end
-  # end
+  admin_authenticator do
+    # Put your admin authentication logic here.
+    redirect_to root_path unless current_user && current_user.admin?
+  end
 
   # Authorization Code expiration time (default 10 minutes).
   # authorization_code_expires_in 10.minutes
