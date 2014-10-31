885add5a0ca498f67bf1341696301c9f7a2978fa
Charlie Maffitt <cmaffitt@highgroove.com>
2014-10-31 13:20:48 -0400

    Buttonize all the things

--- app/models/stop_signal.rb
+++ app/models/stop_signal.rb
@@ -1,4 +1,8 @@
 class StopSignal < ActiveRecord::Base
   belongs_to :start_signal
   validates :stop_time, presence: true
+
+  def elapsed_minutes
+    (stop_time - start_signal.start_time).round(0) / 60
+  end
 end
