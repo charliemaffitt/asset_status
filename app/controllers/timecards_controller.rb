a645c37aa9e9122465dd4311ca7a093f29027b54
Charlie Maffitt <cmaffitt@highgroove.com>
2014-11-03 17:52:49 -0500

    Refactor timecard controller

--- app/controllers/timecards_controller.rb
+++ app/controllers/timecards_controller.rb
@@ -5,42 +5,53 @@ class TimecardsController < ApplicationController
 
   def index
     @active_timecard = current_user.timecards.unpublished.first
-    redirect_to timecard_path(@active_timecard) if @active_timecard.present?
-    # @stop_signals = StopSignal.where('created_at > ?', 1.day.ago)
-    # @stop_signal = @stop_signals.first if @stop_signals
-    # @start_signals = StartSignal.where('created_at > ?', 1.day.ago)
-    # @start_signal = @start_signals.first if @start_signals
-    # @completed_activities = Activity.where('created_at > ?', 5.minutes.ago)
-    # @activity = @completed_activities.first if @completed_activities
+    if @active_timecard.present?
+      redirect_to timecard_path(@active_timecard)
+    else
+      redirect_to new_timecard_path
+    end
+  end
+
+  def new
+    @timecard = current_user.timecards.new
   end
 
   def create
-    @timecard = current_user.timecards.new(location_id: 2, start_time: Time.now)
-    @timecard.save!
-    respond_with @timecard
+    @timecard = current_user.timecards.new(timecard_params.merge(start_time: Time.now))
+    if @timecard.save
+      respond_with @timecard
+    else
+      render 'new'
+    end
   end
 
   def show
-    @timecard = Timecard.find(params[:id])
-    respond_with @timecard
+    respond_with timecard
   end
 
   def update
-    @timecard = Timecard.find(params[:id])
-    @timecard.update_attributes({stop_time: Time.now})
-    respond_with @timecard
+    timecard.update_attributes({stop_time: Time.now})
+    respond_with timecard
   end
 
   def publish
-    @timecard = Timecard.find(params[:id])
-    @activity = Activity.new(user_id: current_user.id, location_id: @timecard.location.id, date: @timecard.stop_time.to_date, hours: @timecard.elapsed_rounded_hours)
-    @timecard.update_attributes({published: true}) if @activity.save!
+    @activity = Activity.new(user_id: current_user.id, location_id: timecard.location.id, date: timecard.stop_time.to_date, hours: timecard.elapsed_rounded_hours)
+    timecard.update_attributes({published: true}) if @activity.save!
     respond_with @activity, location: timecards_path
   end
 
   def destroy
-    @timecard = Timecard.find(params[:id])
-    @timecard.destroy
+    timecard.destroy
     respond_with nil, location: timecards_path
+  end
+
+  private
+
+  def timecard_params
+    params.require(:timecard).permit(:location_id, :start_time, :stop_time)
+  end
+
+  def timecard
+    @timecard ||= Timecard.find(params[:id])
   end
 end
