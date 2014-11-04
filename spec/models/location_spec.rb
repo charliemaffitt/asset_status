500c45ee559551aadcf75dee88980ca6af8f81b2
Charlie Maffitt <cmaffitt@highgroove.com>
2014-11-03 20:19:06 -0500

    Add coverage for total hours spec

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -11,4 +11,20 @@ describe Location do
       expect(FactoryGirl.build(:location, name: nil)).to_not be_valid
     end
   end
+
+  describe ".total_work_hours" do
+
+    let(:location)          { FactoryGirl.create(:location) }
+    let(:second_location)   { FactoryGirl.create(:location)}
+    let!(:first_activity)   { FactoryGirl.create(:activity, hours: 17.0, location: location) }
+    let!(:second_activity)  { FactoryGirl.create(:activity, hours: 3.0, location: location) }
+    let!(:third_activity)   { FactoryGirl.create(:activity, hours: 8.5, location: second_location) }
+
+    it "sums the work hours of all associated activites and returns them as a single floating decimal" do
+      expect(location.total_work_hours).to be_a(Float)
+      expect(location.total_work_hours).to eq 20.0
+      expect(second_location.total_work_hours).to eq 8.5
+    end
+
+  end
 end
