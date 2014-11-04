ec89e17c735d4f0ea20b8d462328aa653415e67a
Charlie Maffitt <cmaffitt@highgroove.com>
2014-11-03 20:15:30 -0500

    Adds spec coverage to total work hours method

--- spec/models/activity_spec.rb
+++ spec/models/activity_spec.rb
@@ -15,4 +15,15 @@ describe Activity do
       expect(FactoryGirl.build(:activity, location: nil)).to_not be_valid
     end
   end
+
+  describe ".total_work_hours" do
+
+    let!(:first_activity)   { FactoryGirl.create(:activity, hours: 17.0) }
+    let!(:second_activity)  { FactoryGirl.create(:activity, hours: 3.0) }
+
+    it "sums the work hours of all instances of the class and returns them as a single floating decimal" do
+      expect(Activity.total_work_hours).to be_a(Float)
+      expect(Activity.total_work_hours).to eq 20.0
+    end
+  end
 end
